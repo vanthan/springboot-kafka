28bfabf49dc5d64892afa84ce5fe21797fa0627c
Vu Van Than <vvthan@cmcglobal.vn>
2022-08-26 13:57:18 +0700

    init project

---
+++ src/main/java/com/vanthan/websocket/VanthanWebsocketSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.vanthan.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanthanWebsocketSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VanthanWebsocketSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vanthan/websocket/VanthanWebsocketSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanthan.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanthanWebsocketSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
